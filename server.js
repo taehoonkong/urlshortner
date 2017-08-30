e23a74af5baee5040a867b4fedd79dc0a7ddd0ae
taehoonkong <dahlia0111@gmail.com>
2017-08-30 16:42:27 +0900

    make form

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const basicAuth = require('express-basic-auth');
 const randomstring = require('randomstring');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -13,6 +14,7 @@ const data = [
   }
 ];
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(basicAuth({
   users: { 'admin': 'admin' },
   challenge: true,
@@ -21,11 +23,36 @@ app.use(basicAuth({
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static('public'));
-// http logger
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {data});
+});
+
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const matched = data.find(item => item.id === id);
+  if(matched) {
+    res.redirect(301, matched.longUrl);
+  } else {
+    res.status(404);
+    res.send('404 Not Found');
+  }
+});
+
+app.post('/', (req, res) => {
+  const longUrl = req.body.longUrl;
+  let id;
+  while(true) {
+    const candidate = randomstring.generate(6);
+    const matched = data.find(item => item.id === candidate);
+    if(!matched) {
+      id = candidate;
+      break;
+    }
+  }
+  data.push({id, longUrl});
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
