b9acfa16ee5c565f760682a39eb877d8067b5d81
taehoonkong <dahlia0111@gmail.com>
2017-08-30 15:42:27 +0900

    data

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const basicAuth = require('express-basic-auth');
 
 const app = express();
 
+const data = [
+  {
+    longUrl: 'http://google.com',
+    id: '58DX37'
+  }
+];
+
 app.use(basicAuth({
-    users: { 'admin': 'admin' },
-    challenge: true,
-    realm: 'Imb4T3st4pp'
+  users: { 'admin': 'admin' },
+  challenge: true,
+  realm: 'Imb4T3st4pp'
 }));
 
 app.set('view engine', 'ejs');
@@ -17,7 +24,7 @@ app.use('/static', express.static('public'));
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index.ejs', {data});
 });
 
 app.listen(3000, () => {
