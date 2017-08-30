5f7d012f4942b41229df5088b4b138da08f3cecb
taehoonkong <dahlia0111@gmail.com>
2017-08-30 15:26:55 +0900

    auth

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 /* jshint esversion: 6 */
 const express = require('express');
+const morgan = require('morgan');
+const basicAuth = require('express-basic-auth');
+
 const app = express();
+
+app.use(basicAuth({
+    users: { 'admin': 'admin' },
+    challenge: true,
+    realm: 'Imb4T3st4pp'
+}));
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static('public'));
+// http logger
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
